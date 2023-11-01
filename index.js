fae6475a198b6b7217426d418d4c4ef6d4b64f83
Sumu <onlystudies888@gmail.comgit commit -m First>
2023-11-01 20:34:18 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// Define the target date (December 1, 2023)
+const targetDate = new Date('2023-12-01T00:00:00');
+
+// Get the current date and time
+const currentDate = new Date();
+
+// Calculate the time difference in milliseconds
+const timeDifference = targetDate - currentDate;
+
+// Calculate days, hours, minutes, and seconds left
+const daysLeft = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+const hoursLeft = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutesLeft = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+const secondsLeft = Math.floor((timeDifference % (1000 * 60) / 1000));
+
+// Time Passed till now
+const newDate = new Date('2023-11-01T00:00:00')
+const passDate = currentDate - newDate
+
+const daysLeft1 = Math.floor(passDate / (1000 * 60 * 60 * 24));
+const hoursLeft1 = Math.floor((passDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutesLeft1 = Math.floor((passDate % (1000 * 60 * 60)) / (1000 * 60));
+const secondsLeft1 = Math.floor((passDate % (1000 * 60) / 1000));
+
+console.log(`Days left: ${daysLeft} days`);
+
+console.log(`Time left: ${hoursLeft} hours, ${minutesLeft} minutes, and ${secondsLeft} seconds`);
+
+
+//formating date
+let days = "Days"
+if (daysLeft < 2 && daysLeft !== 0){
+    days = "Day"
+}
+//formating time
+function addingZero(num){
+    if (num < 10){
+        return '0'+num
+    }else{
+        return num
+    }
+}
+let hours = addingZero(hoursLeft)
+let minutes = addingZero(minutesLeft)
+let seconds = addingZero(secondsLeft)
+
+//formating Output
+let txtHour = 'Hours'
+let txtMinute = 'Minutes'
+let TextSecond = 'Seconds'
+
+
+//html output
+const data = document.getElementById('dayLeft')
+data.textContent = `${daysLeft} ${days} ${hours} ${txtHour} ${minutes} ${txtMinute} ${seconds} ${TextSecond}`
+
+
+const data1 = document.getElementById('dayPassed')
+data1.textContent = `${daysLeft1} Days ${hoursLeft1} Hours ${minutesLeft1} Minutes ${secondsLeft1} Seconds`
+//refresh webiste
+// Function to reload the page every second
+function refreshPage() {
+    location.reload();
+  }
+  
+  // Set a timeout to call the refreshPage function every 1000 milliseconds (1 second)
+  setInterval(refreshPage, 100000);
+  
